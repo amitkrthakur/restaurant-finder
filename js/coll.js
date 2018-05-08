7f024eb4cd3102ddfb224ac20a576801347c5bf6
Amit Kumar <amkt18@live.com>
2018-05-08 19:09:30 +0530

    uploading to git repo

---
+++ js/coll.js
@@ -0,0 +1,30 @@
+//Zomato API-Collections search js-codes to automate HTML creation using handlebars.js
+
+
+var httpreq = "collections?lat=28.4213889&lon=77.326699";
+var UserRequest = {
+  "async": true,
+  "crossDomain": true,
+  "url": "https://developers.zomato.com/api/v2.1/"+httpreq+"",
+  "method": "GET",
+  "headers": {
+  "user-key": "3c99a91c83aaebabc4ee9ca71bf6dba4",
+  }
+}
+
+$.ajax(UserRequest).done(function (response) {
+    data = response;
+    console.log(data);
+    createHTML();
+
+})
+
+
+function createHTML(){
+var rawTemplate = document.getElementById("template").innerHTML;
+var compiledTemplate = Handlebars.compile(rawTemplate);
+var generatedHTML = compiledTemplate(data);
+    
+var writeData = document.getElementById("write-data");
+writeData.innerHTML = generatedHTML;
+}
